9d3cb5e1fc2b01086d27488870b5c99bb788e598
Athar Laique <atharlaique15@gmail.com>
2020-07-24 23:33:40 +0500

    Controller Folder position change

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,7 +1,7 @@
 const express=require('express');
 const route=express.Router();
 //Controller
-const {signup} =require('../../controller/auth-controller');
+const {signup} =require('../controller/auth-controller');
 
 //Get
 route.get('/signup',signup)
