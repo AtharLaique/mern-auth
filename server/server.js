94c36621d25d4303cfa3a144b391127c6ea531fa
Athar Laique <atharlaique15@gmail.com>
2020-07-27 06:07:29 +0500

    Morgan logger

--- server/server.js
+++ server/server.js
@@ -1,10 +1,14 @@
 const express=require("express");
 const app=express();
-require('dotenv').config()
+const morgan=require('morgan');
+require('dotenv').config();
+
 const port =process.env.PORT || 3001
 
 //Routes
 const authRoute=require('./routes/auth');
+// App Middlewares
+app.use(morgan('dev'))
 //Middleware
 app.use('/api/auth',authRoute);
 app.listen(port,()=>{
