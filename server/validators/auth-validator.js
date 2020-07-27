1ce1c9fd087903e7aa11b8e6348e4206351a9f72
Athar Laique <atharlaique15@gmail.com>
2020-07-27 13:58:29 +0500

    validator defination

---
+++ server/validators/auth-validator.js
@@ -0,0 +1,13 @@
+const {check}=require('express-validator');
+exports.signupValidatores=[
+    check('name')
+    .not()
+    .isEmpty()
+    .withMessage("Name is required"),
+    check('email')
+    .isEmail()
+    .withMessage("enter a valid email"),
+    check('password')
+    .isLength({min:6})
+    .withMessage("password must be 6 chracter long")
+]
