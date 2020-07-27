1820d8bc235d8e16e0d0b9658100e3f2d8ac9cb5
Athar Laique <atharlaique15@gmail.com>
2020-07-27 06:14:09 +0500

    bodyParser

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express=require("express");
 const app=express();
 const morgan=require('morgan');
 const cors=require('cors');
+const bodyParser=require('body-parser');
 require('dotenv').config();
 
 const port =process.env.PORT || 3001
@@ -9,10 +10,13 @@ const port =process.env.PORT || 3001
 //Routes
 const authRoute=require('./routes/auth');
 // App Middlewares
+
 app.use(morgan('dev'));
 if(process.env.NODE_ENV=='development'){
     app.use(cors({origin:'http://localhost:3000'}))
 }
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}))
 //Middleware
 app.use('/api/auth',authRoute);
 app.listen(port,()=>{
