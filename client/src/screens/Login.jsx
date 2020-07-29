2b731e8b05fe79f6aa86f3ebeddf0844eb792d74
Athar Laique <atharlaique15@gmail.com>
2020-07-29 15:24:22 +0500

    Login and signup screen

---
+++ client/src/screens/Login.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Layout from "../components/Layout";
+import Card from "../components/Card";
+
+const Login = () => {
+  return (
+    <Layout>
+      <div class="row">
+        <div className="col-sm-3 col-md-4 col-lg-4"></div>
+        <div className="col-sm-6 col-md-4 col-lg-4">
+            <Card title="Login">
+
+            </Card>
+        </div>
+        <div className="col-sm-3 col-md-4 col-lg-4"></div>
+      </div>
+    </Layout>
+  );
+};
+export default Login;
