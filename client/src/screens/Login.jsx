3114606aeb943724d133a768ac49ebe510891caa
Athar Laique <atharlaique15@gmail.com>
2020-07-29 15:41:17 +0500

    Login and signup

--- client/src/screens/Login.jsx
+++ client/src/screens/Login.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Layout from "../components/Layout";
 import Card from "../components/Card";
+import Input from "../components/Input";
+import Button from "../components/Button";
 
 const Login = () => {
   return (
@@ -9,7 +11,21 @@ const Login = () => {
         <div className="col-sm-3 col-md-4 col-lg-4"></div>
         <div className="col-sm-6 col-md-4 col-lg-4">
             <Card title="Login">
-
+                <Input 
+                label="Email"
+                placeholder="Enter Your Email ..."
+                id="email"
+                name="email"
+                type="email"
+                />
+                 <Input 
+                label="Password"
+                placeholder="Enter Your Password ..."
+                id="password"
+                name="password"
+                type="password"
+                />
+                    <Button title="Go!"/>
             </Card>
         </div>
         <div className="col-sm-3 col-md-4 col-lg-4"></div>
--- client/src/screens/Signup.jsx
+++ client/src/screens/Signup.jsx
@@ -1,11 +1,42 @@
-import React from 'react';
-import Layout from '../components/Layout';
-
-const Signup=()=> {
-    return (
-        <Layout>
-            <h1>Its Signup</h1>
-        </Layout>
-    )
-}
+import React from "react";
+import Layout from "../components/Layout";
+import Card from "../components/Card";
+import Input from "../components/Input";
+import Button from "../components/Button";
+const Signup = () => {
+  return (
+    <Layout>
+      <div class="row">
+        <div className="col-sm-3 col-md-4 col-lg-4"></div>
+        <div className="col-sm-6 col-md-4 col-lg-4">
+          <Card title="Signup">
+          <Input 
+                label="Name"
+                placeholder="Enter Your Name ..."
+                id="name"
+                name="name"
+                type="name"
+                />
+          <Input 
+                label="Email"
+                placeholder="Enter Your Email ..."
+                id="email"
+                name="email"
+                type="email"
+                />
+                 <Input 
+                label="Password"
+                placeholder="Enter Your Password ..."
+                id="password"
+                name="password"
+                type="password"
+                />
+                <Button title="Create Acount !"/>
+          </Card>
+        </div>
+        <div className="col-sm-3 col-md-4 col-lg-4"></div>
+      </div>
+    </Layout>
+  );
+};
 export default Signup;
