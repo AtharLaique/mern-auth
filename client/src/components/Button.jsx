d4fd884e506807fc4d985a1fd024bfd468f15133
Athar Laique <atharlaique15@gmail.com>
2020-07-29 15:41:08 +0500

    Create Button.jsx

---
+++ client/src/components/Button.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Button=({title})=> {
+    return (
+    <button type="button" class="btn btn-primary btn-lg btn-block">
+        {title}
+    </button>
+    )
+}
+export default Button;
